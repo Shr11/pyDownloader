e1be760a15ce005d6d206a7da5963d5d3c572031
MageXstarLynn <shr.comp1@gmail.com>
2023-05-07 18:20:20 +0530

    simple calculator

---
+++ Q4.py
@@ -0,0 +1,11 @@
+# simple calculator for two no.s
+
+a, b = map(int,input("Enter two no.s\n ").split())
+
+print("sum :\n", a + b)
+
+print("subtraction :\n" , a - b)
+
+print("Division :\n" , a/b)
+
+print("Multiplication :\n"  , a*b)
