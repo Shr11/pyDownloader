c12f6ee22bdb780ec5323a3db542912f78b6b2b5
MageXstarLynn <shr.comp1@gmail.com>
2023-05-07 17:22:51 +0530

    assignment

--- Q2.py
+++
@@ -1,8 +0,0 @@
-#program to show indentation error 
-
-
-print("hello world")
-
-# indentation error 
- for i in range(5):
- print("*", end = " ")
--- Q3.py
+++
@@ -1,8 +0,0 @@
-#program to correct previous error
-
-
-print("hello world")
-
-# indentation error  CORRECTED!!
-for i in range(5):
- print("*", end = " ")
