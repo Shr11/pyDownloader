9ccac57cd2253c09fe1be60459c5eb7530e7b764
MageXstarLynn <shr.comp1@gmail.com>
2023-05-07 17:58:52 +0530

    using input function

---
+++ Q3.py
@@ -0,0 +1,9 @@
+#input function
+
+name = input("Enter name: \n")
+
+age = int(input("\n Enter your age\t"))
+
+cgpa = float(input("\nEnter your cgpa\n"))
+
+print("You are:", name, age, "yrs old and have ", cgpa ," cgpa.") 
